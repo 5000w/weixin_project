e55f802481fac56e612f3d60be3ade06ba71cc0c
liangxiaoguang <liangtian68@outlook.com>
2018-09-25 11:51:02 +0800

    test

--- code/weixin_market/scripts/goods.py
+++ code/weixin_market/scripts/goods.py
@@ -11,12 +11,23 @@ def add_goods_car(id,goods_id):
     user.car_info_set.create(goods=goods)
 
 #查看所有商品
+def get_all_goods():
+    return [{'name' : x.goods_name,'count' : x.count , 'url' : x.image_url }  for x in Goods.objects.filter()]
+
 
 #查看某个openid购物车的商品
 def get_goods(id):
     user = Weixin_user.objects.get(id=id)
-    return user.car_info_set.values('goods')
+    return [x for x in user.car_info_set.values('goods')]
+
+#删除购物车中商品
+def del_car(id,goods_id_list):
+
+    for goods_id in goods_id_list():
+        user = Weixin_user.objects.get(id=id)
+        goods = Goods.objects.get(id=goods_id)
+        user.car_info_set.filter(goods=goods).delete()
 
 def run():
     #add_goods_car(3,2)
-    print(get_goods(3))
+    #print(get_goods(3))
