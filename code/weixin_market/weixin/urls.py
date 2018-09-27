d8dfc3d3ce5a19aacc34e391aa8a55dc57dcd99e
ubuntu <ubuntu@localhost.localdomain>
2018-09-27 21:55:07 +0800

    urls update

--- code/weixin_market/weixin/urls.py
+++ code/weixin_market/weixin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_coupon', views.user_login, name='get_coupon'),
-    path('login', views.get_coupon, name='login'),
+    path('login', views.user_login, name='login'),
+    path('get_coupon', views.get_coupon, name='get_coupon'),
     path('set_coupon_sta', views.set_coupon_sta, name='set_coupon_sta'),
 ]
