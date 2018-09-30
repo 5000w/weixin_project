cd8f3cfd0654a5315717b56a40fb44c6a78955ad
ubuntu <ubuntu@localhost.localdomain>
2018-09-30 15:24:06 +0800

    zhifu

--- code/weixin_market/weixin/urls.py
+++ code/weixin_market/weixin/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('login', views.user_login, name='login'),
     path('get_coupon', views.get_coupon, name='get_coupon'),
     path('set_coupon_sta', views.set_coupon_sta, name='set_coupon_sta'),
+    path('get_all_goods', views.get_all_goods, name='get_all_goods'),
+    path('payOrder', views.payOrder, name='payOrder'),
+    path('payback', views.payback, name='payback'),
 ]
