40c00417754ea1b534a22b7e1c1fef1cdcd9ff86
liangxiaoguang <liangtian68@outlook.com>
2018-09-30 15:54:53 +0800

    API DOC

--- code/weixin_market/weixin/urls.py
+++ code/weixin_market/weixin/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('set_coupon_sta', views.set_coupon_sta, name='set_coupon_sta'),
 
     path('share_for_coupon',views.share_for_coupon,name='share_for_coupon'),
-
+    path('get_class',views.get_class,name='get_class'),
     path('get_all_goods', views.get_all_goods, name='get_all_goods'),
     path('payOrder', views.payOrder, name='payOrder'),
     path('payback', views.payback, name='payback'),
