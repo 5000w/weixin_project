71518b7d0de81d3624520e52cbe98520e9fd292e
liangxiaoguang <liangtian68@outlook.com>
2018-10-18 14:29:20 +0800

    fix bug

--- code/weixin_market/scripts/order.py
+++ code/weixin_market/scripts/order.py
@@ -80,12 +80,12 @@ def get_order_bytxt():
         if x.type == 1 :
             file.write("{4} {0} {1} {2} {3}\r\n".format(x.school_name,x.phone_number,x.pwd,x.class_name,'智慧树'))
             count=count+len(x.class_name.split(','))
-            file.write("总计：" + str(count))
+
         else:
             file.write("{4} {0} {1} {2} {3}\r\n".format(x.school_name, x.phone_number, x.pwd, x.class_name, '超星'))
             count = count + len(x.class_name.split(','))
-            file.write("总计：" + str(count))
 
+    file.write("总计：" + str(count))
     file.write("---------------------------------------------------------")
 
     file.close()
